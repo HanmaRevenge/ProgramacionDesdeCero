d816fb80f9a57d6ef26bb15571fc0174f8055fe1
Daniel <djesrui07@gmail.com>
2024-03-04 13:58:29 +0100

    He subido el primer mini-proyecto básico.

---
+++ .src/PrimeraClase.java
@@ -0,0 +1,11 @@
+public class PrimeraClase {
+
+    //"Abro un Main para que sea una Clase Ejecutable"//
+    public static void main(String[] args) {
+
+        //"Abro un Sout para escribir un Mensaje por Máquina"//
+
+        System.out.println("Hola, amigos/as");
+
+    }
+}
